03de37654bc7f566c36f455b86d26075e65b248d
Hyun <iamhyunji_kim@icloud.com>
2021-02-18 16:02:45 +0900

    현지 : 완주하지 못한 선수 효율성 0점..

--- src/Day1/모의고사/HJ.java
+++ src/Day1/모의고사/HJ.java
@@ -2,8 +2,22 @@ package Day1.모의고사;
 
 public class HJ {
 
+	public static int[] solution(int[] answers) {
+        int[] answer = {};
+        int[] p1 = {1, 2, 3, 4, 5};
+        int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
+        for(int i : answers) {
+        	
+        }
+        
+        return answer;
+    }
+	
 	public static void main(String[] args) {
-		System.out.println("test");
+		int[] answers = {1,2,3,4,5};
+		System.out.println(solution(answers));
 	}
 
 }
