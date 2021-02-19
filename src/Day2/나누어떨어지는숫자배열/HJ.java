d9493b1e73ae0832fac1c09a93946ddfc2b79cc0
Hyun <iamhyunji_kim@icloud.com>
2021-02-19 21:39:01 +0900

    현지 : Day2 숙제 끝!

--- src/Day2/나누어떨어지는숫자배열/HJ.java
+++ src/Day2/나누어떨어지는숫자배열/HJ.java
@@ -1,12 +1,67 @@
 package Day2.나누어떨어지는숫자배열;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class HJ {
 	public static void main(String[] args) {
+		int[] arr = { 2, 36, 1, 3 };
+		int divisor = 7;
 
+		System.out.println(solution(arr, divisor));
 	}
-	
+
 	public static int[] solution(int[] arr, int divisor) {
-        int[] answer = {};
-        return answer;
-    }
+		int[] answer = {};
+		List<Integer> list = new ArrayList<>();
+
+		// 정렬 : 버블
+		for (int i = 0; i < arr.length; i++)
+			for (int j = 0; j < arr.length -i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+
+		System.out.println(Arrays.toString(arr));
+		
+		// 숫자 구하기
+		for (int element : arr) {
+			if (element % divisor == 0)
+				list.add(element);
+		}
+
+		// 배열로 변경
+		answer = list.stream().mapToInt(i -> i).toArray();
+		if (answer.length == 0) {
+			answer = new int[1];
+			Arrays.fill(answer, -1);
+		}
+
+		return answer;
+	}
+
+//	public static int[] solution(int[] arr, int divisor) {
+//		int[] answer = {};
+//		List<Integer> list = new ArrayList<>();
+//		Arrays.sort(arr);
+//		
+//		// 숫자 구하기
+//		for(int element : arr) {
+//			if(element % divisor == 0)
+//				list.add(element);
+//		}
+//		
+//		// 배열로 변경
+//		answer = list.stream().mapToInt(i -> i).toArray();
+//		if(answer.length == 0) {
+//			answer = new int[1];
+//			Arrays.fill(answer, -1);
+//		}
+//
+//		return answer;
+//	}
 }
