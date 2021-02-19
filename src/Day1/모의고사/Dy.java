ab00f9d5205d868368ec010e06f7b9bb4bf6d649
kdyddd <kdyddd@gmail.com>
2021-02-19 09:35:59 +0900

    동영 : 1일차

---
+++ src/Day1/모의고사/Dy.java
@@ -0,0 +1,62 @@
+package Day1.모의고사;
+
+public class Dy {
+
+	public int[] solution(int[] answers) {
+		
+		int[] answer = {};
+        int[] first = {1,2,3,4,5};
+        int[] second = {2,1,2,3,2,4,2,5};
+        int[] third = {3,3,1,1,2,2,4,4,5,5};
+        
+        int[] points = {0, 0, 0};
+        int[] members = {1, 2, 3};
+        int winner = 0;
+        
+        for(int i = 0; i<answers.length; i++) {
+        	if(answers[i] == first[i % first.length])
+        		points[0]++;
+        	if(answers[i] == second[i % second.length])
+        		points[1]++;
+        	if(answers[i] == third[i % third.length])
+        		points[2]++;
+        }
+        
+        for(int i=0; i<points.length-1; i++) {
+        	int temp;
+        	if(points[i] < points[i+1]) {
+        		temp = points[i];
+        		points[i] = points[i+1];
+        		points[i+1] = temp;
+        		temp = members[i];
+        		members[i] = members[i+1];
+        		members[i+1] = temp;
+        	}
+        		
+        }
+        
+        for(int i=0; i<points.length; i++)
+        	if(points[0] == points[i])
+        		winner++;
+        if(points[0] == 0 && points[1] == 0 && points[2] == 0)
+            return answer;
+        
+        answer = new int[winner];
+        for(int i=0; i<winner; i++)
+        	answer[i] = members[i];
+        
+        for(int i=0; i<answer.length-1; i++) {
+        	for(int j=0; j<answer.length-1-i; j++){
+                int temp;
+                if(answer[j] > answer[j+1]) {
+                    temp = answer[j];
+                    answer[j] = answer[j+1];
+                    answer[j+1] = temp;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+}
---
+++ src/Day1/완주하지못한선수/Dy.java
@@ -0,0 +1,24 @@
+package Day1.완주하지못한선수;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dy {
+
+	public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for(String key : participant)
+        	map.put(key, map.getOrDefault(key, 0)+1);
+        
+        for(String key : completion)
+        	map.put(key, map.getOrDefault(key, 0)-1);
+        
+        for(String key : participant)
+        	if(map.get(key) != 0)
+        		answer = key;
+        
+        return answer;
+    }
+
+}
