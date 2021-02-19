967a02883a462e59c43cdc8fce5697499fbdf8df
Hyun <iamhyunji_kim@icloud.com>
2021-02-19 21:12:19 +0900

    현지 : 마지막 문제 시작

--- src/Day2/같은숫자는싫어/HJ.java
+++ src/Day2/같은숫자는싫어/HJ.java
@@ -1,7 +1,28 @@
 package Day2.같은숫자는싫어;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HJ {
 	public static void main(String[] args) {
+		int[] arr = { 4, 4, 4, 3, 3 };
+		System.out.println(solution(arr));
+	}
 
+	public static int[] solution(int[] arr) {
+		int criteria = arr[0];
+
+		List<Integer> list = new ArrayList<>();
+		list.add(criteria);
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == criteria) {
+				continue;
+			}
+			criteria = arr[i];
+			list.add(arr[i]);
+		}
+
+		return list.stream().mapToInt(i -> i).toArray();
 	}
 }
---
+++ src/Day2/나누어떨어지는숫자배열/HJ.java
@@ -0,0 +1,12 @@
+package Day2.나누어떨어지는숫자배열;
+
+public class HJ {
+	public static void main(String[] args) {
+
+	}
+	
+	public static int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
+        return answer;
+    }
+}
