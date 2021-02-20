fa8a37fcc9a459d521265b68270d673d2338815d
Hyun <iamhyunji_kim@icloud.com>
2021-02-20 16:55:14 +0900

    현지 : 심심풀이용

---
+++ src/Day0/삼진법뒤집기/HJ.java
@@ -0,0 +1,13 @@
+package Day0.삼진법뒤집기;
+
+public class HJ {
+	public static void main(String[] args) {
+		int n = 45;
+		System.out.println(solution(n));
+	}
+
+	public static int solution(int n) {
+		int answer = 0;
+		return answer;
+	}
+}
