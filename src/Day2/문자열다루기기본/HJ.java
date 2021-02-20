5a3cb2900f0e867018402f981299e6a2de29fe5c
Hyun <iamhyunji_kim@icloud.com>
2021-02-20 16:07:27 +0900

    현지 : 끝!

--- src/Day2/문자열다루기기본/HJ.java
+++ src/Day2/문자열다루기기본/HJ.java
@@ -1,5 +1,24 @@
 package Day2.문자열다루기기본;
 
 public class HJ {
+	public static void main(String[] args) {
+		String s = "1234";
+		System.out.println(solution(s));
+	}
 
+	public static boolean solution(String s) {
+		boolean answer = false;
+
+		if(s.length() != 4 && s.length() != 6)
+			return answer;
+		
+		try {
+			Integer.parseInt(s);
+			answer = true;
+		} catch(Exception e) {
+			return answer;
+		}
+		
+		return answer;
+	}
 }
