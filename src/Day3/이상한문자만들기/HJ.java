5a88a8f3b8bf509790be800d6187208298451e82
Hyun <iamhyunji_kim@icloud.com>
2021-02-22 19:34:11 +0900

    현지 : Day3 패키지 생성

---
+++ src/Day3/이상한문자만들기/HJ.java
@@ -0,0 +1,14 @@
+package Day3.이상한문자만들기;
+
+public class HJ {
+	public static void main(String[] args) {
+		String s = "try hello world";
+		System.out.println(solution(s));
+	}
+	
+	public static String solution(String s) {
+        String answer = "";
+        return answer;
+    }
+	
+}
