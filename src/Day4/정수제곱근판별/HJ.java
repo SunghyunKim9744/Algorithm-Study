ab28d1319d3957804ea8941cd24f1404759d3977
Hyun <iamhyunji_kim@icloud.com>
2021-02-23 22:41:58 +0900

    현지 : Day4 - 7/8

--- src/Day4/정수제곱근판별/HJ.java
+++ src/Day4/정수제곱근판별/HJ.java
@@ -2,12 +2,43 @@ package Day4.정수제곱근판별;
 
 public class HJ {
 	public static void main(String[] args) {
-		long n = 50000000000000L;
+		long n = 121;// 50000000000000L;
 		System.out.println(solution(n));
 	}
 
-	public static long solution(long n) {
-		long answer = 0;
-		return answer;
-	}
+//	// 방법 1-1) (0.03ms, 53.6MB) - (19.11ms, 53.7MB)
+//	public static long solution(long n) {
+//		long answer = 0;
+//
+//		for (int i = 1; i <= n / 2; i++)
+//			if (Math.sqrt(n) == (double) i) {
+//				answer = i;
+//				break;
+//			}
+//
+//		answer = (answer == 0) ? -1 : (long) Math.pow(answer + 1, 2);
+//		
+//		return answer;
+//	}
+	
+	// 방법 1-2) (0.03ms, 53.6MB) - (19.11ms, 53.7MB)
+		public static long solution(long n) {
+			
+			return (long) (Math.pow(Math.sqrt(n), 2) == n ? Math.pow(Math.sqrt(n) + 1, 2) : -1);
+		}
+
+//	// 방법 2) 통과 (0.90ms, 53.1MB) - (시간 초과)
+//	public static long solution(long n) {
+//		long answer = 0;
+//
+//		for (int i = 1; i <= n / 2; i++)
+//			if (i * i == n) {
+//				answer = i;
+//				break;
+//			}
+//		
+//		answer = (answer == 0) ? -1 : (answer + 1) * (answer + 1);
+//		
+//		return answer;
+//	}
 }
--- src/Day4/최대공약수와최소공배수/HJ.java
+++ src/Day4/최대공약수와최소공배수/HJ.java
@@ -1,12 +1,15 @@
 package Day4.최대공약수와최소공배수;
 
+import java.util.Arrays;
+
 public class HJ {
 	public static void main(String[] args) {
 		int n = 1000000;
 		int m = 1000000;
-		System.out.println(solution(n, m));
+		System.out.println(Arrays.toString(solution(n, m)));
 	}
 
+	// (1.66ms, 52.1MB) - (4.21ms, 52.5MB)
 	public static int[] solution(int n, int m) {
 		int[] answer = {};
 		return answer;
