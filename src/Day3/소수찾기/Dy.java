4520893f25b1dbec26a03b72c1027f5a22900267
kdyddd <kdyddd@gmail.com>
2021-02-23 09:33:17 +0900

    동영 : day3

---
+++ src/Day3/소수찾기/Dy.java
@@ -0,0 +1,43 @@
+package Day3.소수찾기;
+
+public class Dy {
+	
+//	public int solution(int n) {
+//		int answer = 0;
+//		answer = n-1;
+//        for(int i = 2; i<n+1; i++) {
+//        	for(int j = 2; j<i; j++) {
+//        		if(i % j == 0) {
+//        			answer--;
+//        			break;
+//        		}
+//        	}
+//        }
+//        
+//        return answer;
+//    }
+	
+//	public int solution(int n) {
+//        int answer = 0;
+//		List<Integer> list = new ArrayList<Integer>();
+//		list.add(2);
+//		boolean addNum = true;
+//		for(int i=3; i<n+1; i++) {
+//			for(int j=0; j<list.size(); j++) {
+//				if(i % list.get(j) == 0) {
+//					addNum = false;
+//					break;
+//				}
+//			}
+//			
+//			if(addNum)
+//				list.add(i);
+//			
+//			addNum =true;
+//		}
+//        answer = list.size();
+//        
+//        return answer;
+//    }
+	
+}
---
+++ src/Day3/시저암호/Dy.java
@@ -0,0 +1,18 @@
+package Day3.시저암호;
+
+public class Dy {
+
+	public String solution(String s, int n) {
+        String answer = "";
+        for(int i = 0; i<s.length(); i++){
+           if(s.charAt(i) != " " && s.charAt(i) < 91){
+               s.charAt(i) = ((s.charAt(i) + n - 64) % 23) + 64;
+           }else if(s.charAt(i) != " " && s.charAt(i) > 91){
+               s.charAt(i) = ((s.charAt(i) + n - 96) % 23) + 96;
+           }
+        }
+        answer = s;
+        
+        return answer;
+    }
+}
