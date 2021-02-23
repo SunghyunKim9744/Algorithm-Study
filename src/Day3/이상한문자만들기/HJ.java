8266b71f262e24bc6a34657288370e25a0487f27
Hyun <iamhyunji_kim@icloud.com>
2021-02-23 09:30:33 +0900

    현지 : Day3

--- src/Day3/이상한문자만들기/HJ.java
+++ src/Day3/이상한문자만들기/HJ.java
@@ -1,24 +1,32 @@
 package Day3.이상한문자만들기;
 
+/*
+ *  각 단어는 하나 이상의 공백문자로 구분
+ */
+
 public class HJ {
 	public static void main(String[] args) {
-		String s = "tRy hello world";
+		String s = "tRy hello      world    a";
 		System.out.println(solution(s));
 	}
 
 	public static String solution(String s) {
 		String answer = "";
-		s = s.toLowerCase();
-		String[] words = s.split(" ");
+		String[] words = s.split("\\s{1,}");
 
-		for (int i=0; i < words.length; i++) {
-			String[] charArr = words[i].split("");
+		for (int i = 0; i < words.length; i++) {
+			char[] charArr = words[i].toCharArray();
+			
 			for (int j = 0; j < charArr.length; j++)
 				if (j % 2 == 0)
-					charArr[j] = charArr[j].toUpperCase();
-			words[i] = String.join("",charArr);
+					charArr[j] = Character.toUpperCase(charArr[j]);
+				else
+					charArr[j] = Character.toLowerCase(charArr[j]);
+			
+			words[i] = String.valueOf(charArr);
+			System.out.println(words[i]);
 		}
-
+		
 		answer = String.join(" ", words);
 		return answer;
 	}
