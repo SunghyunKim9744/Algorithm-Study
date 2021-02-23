fff4b2da479917f2ae46aa4ffbecf48e522e182e
kdyddd <kdyddd@gmail.com>
2021-02-23 22:23:31 +0900

    동영 : 주석

--- src/Day3/시저암호/Dy.java
+++ src/Day3/시저암호/Dy.java
@@ -2,17 +2,17 @@ package Day3.시저암호;
 
 public class Dy {
 
-	public String solution(String s, int n) {
-        String answer = "";
-        for(int i = 0; i<s.length(); i++){
-           if(s.charAt(i) != " " && s.charAt(i) < 91){
-               s.charAt(i) = ((s.charAt(i) + n - 64) % 23) + 64;
-           }else if(s.charAt(i) != " " && s.charAt(i) > 91){
-               s.charAt(i) = ((s.charAt(i) + n - 96) % 23) + 96;
-           }
-        }
-        answer = s;
-        
-        return answer;
-    }
+//	public String solution(String s, int n) {
+//        String answer = "";
+//        for(int i = 0; i<s.length(); i++){
+//           if(s.charAt(i) != " " && s.charAt(i) < 91){
+//               s.charAt(i) = ((s.charAt(i) + n - 64) % 23) + 64;
+//           }else if(s.charAt(i) != " " && s.charAt(i) > 91){
+//               s.charAt(i) = ((s.charAt(i) + n - 96) % 23) + 96;
+//           }
+//        }
+//        answer = s;
+//        
+//        return answer;
+//    }
 }
