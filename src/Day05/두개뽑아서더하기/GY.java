aa15a2139b50bd8300acadbb382c6f632bc212d7
aengun <aengun@icloud.com>
2021-02-25 02:09:51 +0900

    건영 : Day5 완료

--- src/Day05/두개뽑아서더하기/GY.java
+++ src/Day05/두개뽑아서더하기/GY.java
@@ -1,9 +1,40 @@
 package Day05.두개뽑아서더하기;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
 public class GY {
 
 	public static void main(String[] args) {
+		int[] nums1 = { 2, 1, 3, 4, 1 };
+		int[] nums2 = { 5, 0, 2, 7 };
 
+		solution(nums1);
+		System.out.println();
+		solution(nums2);
 	}
-	
+
+	public static int[] solution(int[] numbers) {
+		
+		int[] answer = {};
+		// 배열을 리스트로 변환
+		List<Integer> newNumbers = Arrays.stream(numbers).boxed().collect(Collectors.toList());
+		// 중복 제거를 위해 Set 정의, TreeSet은 오름차순으로 자동 정렬해줌
+		Set<Integer> set = new TreeSet<>();
+		// list에서 다른 인덱스에 있는 값을 더해 set에 담기
+		for (int i = 0; i < newNumbers.size(); i++)
+			for (int j = 0; j < newNumbers.size(); j++)
+				if (i != j)
+					set.add(newNumbers.get(i) + newNumbers.get(j));
+		// set을 list로 변환
+		List<Integer> sumOfNumbers = new ArrayList<>(set);
+		// list를 array로 변환
+		answer = set.stream().mapToInt(i->i).toArray();
+
+		return answer;
+	}
 }
--- src/Day05/크레인인형뽑기게임/GY.java
+++ src/Day05/크레인인형뽑기게임/GY.java
@@ -1,9 +1,50 @@
 package Day05.크레인인형뽑기게임;
 
+import java.util.Stack;
+
 public class GY {
 
 	public static void main(String[] args) {
-
+		int[][] board = { { 0, 0, 0, 0, 0 }, 
+						  { 0, 0, 1, 0, 3 }, 
+						  { 0, 2, 5, 0, 1 }, 
+						  { 4, 2, 4, 4, 2 },
+						  { 3, 5, 1, 3, 1 } };
+		int[] moves = { 1, 5, 3, 5, 1, 2, 1, 4 };
+		System.out.println(solution(board, moves));
 	}
-	
+
+	public static int solution(int[][] board, int[] moves) {
+		int answer = 0;
+		
+		Stack<Integer> stack = new Stack<>();
+
+		for (int i = 0; i < moves.length; i++) {
+			
+			int move = moves[i] - 1;
+			for (int j = 0; j < board[0].length; j++) {
+				
+				if (board[j][move] != 0) {
+					
+					boolean done = false;
+					// 스택에 같은 값이 있으면 push하지 않고 바로 삭제
+					if (!stack.isEmpty() && stack.peek() == board[j][move]) {
+						stack.pop();
+						answer += 2;
+						done = true;
+						board[j][move] = 0;
+					}
+					// 삭제된 경우 반복문 빠져나가기
+					if (done)
+						break;
+					// 삭제할 값이 없는 경우 push
+					stack.push(board[j][move]);
+					board[j][move] = 0;
+					break;
+				}
+			}
+		}
+
+		return answer;
+	}
 }
