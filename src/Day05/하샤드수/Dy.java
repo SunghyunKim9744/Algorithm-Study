b655ce0f504d802d8b86dbddcea7684092b599f0
kdyddd <kdyddd@gmail.com>
2021-02-25 06:21:51 +0900

    동영 : day5 완료

---
+++ src/Day05/하샤드수/Dy.java
@@ -0,0 +1,19 @@
+package Day05.하샤드수;
+
+public class Dy {
+
+	public boolean solution(int x) {
+        boolean answer = true;
+        int sum = 0;
+        int num = x;
+        while(num>0){
+            sum += num % 10;
+            num /= 10;
+        }
+        
+        if(x % sum != 0)
+            answer = false;
+        
+        return answer;
+    }
+}
