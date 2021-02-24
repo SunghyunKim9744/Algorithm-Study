dfbc63522b3c7727716b73cbd47e612533e8c199
aengun <aengun@icloud.com>
2021-02-24 12:01:15 +0900

    건영 : day3 패키지 변경

---
+++ src/Day03/문자열을정수로바꾸기/Dy.java
@@ -0,0 +1,10 @@
+package Day03.문자열을정수로바꾸기;
+
+public class Dy {
+
+	public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
---
+++ src/Day03/문자열을정수로바꾸기/GY.java
@@ -0,0 +1,15 @@
+package Day03.문자열을정수로바꾸기;
+
+public class GY {
+
+	public static void main(String[] args) {
+		System.out.println(solution("1234"));
+	}
+	
+	public static int solution(String s) {
+        
+        return Integer.parseInt(s);
+        
+    }
+	
+}
