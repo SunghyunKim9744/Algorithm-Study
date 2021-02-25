d96bf97f0254abfc38c7e75005d4669d8e28916d
Hyun <iamhyunji_kim@icloud.com>
2021-02-25 11:12:44 +0900

    현지 : Day05 인형뽑기...

--- src/Day05/두개뽑아서더하기/HJ.java
+++ src/Day05/두개뽑아서더하기/HJ.java
@@ -1,5 +1,24 @@
 package Day05.두개뽑아서더하기;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HJ {
+	public static void main(String[] args) {
+		int[] numbers = { 2, 1, 3, 4, 1 };
+		System.out.println(solution(numbers));
+	}
+
+	// 방법 1) (5.38ms, 52.2MB) - (18.14ms, 52.5MB)
+	public static int[] solution(int[] numbers) {
+		Set<Integer> set = new HashSet<>();
+
+		for (int i = 0; i < numbers.length; i++)
+			for (int j = 0; j < numbers.length; j++)
+				if (i != j)
+					set.add(numbers[i] + numbers[j]);
+
+		return set.stream().sorted().mapToInt(i -> i).toArray();
+	}
 
 }
--- src/Day05/크레인인형뽑기게임/HJ.java
+++ src/Day05/크레인인형뽑기게임/HJ.java
@@ -1,5 +1,47 @@
 package Day05.크레인인형뽑기게임;
 
-public class HJ {
+import java.util.List;
+import java.util.Arrays;
+import java.util.LinkedList;
 
+public class HJ {
+	public static void main(String[] args) {
+		int[][] board = { { 0, 0, 3, 3, 3 }, { 0, 0, 1, 0, 3 }, { 0, 2, 5, 0, 1 }, { 4, 2, 4, 4, 2 },
+				{ 3, 5, 1, 3, 1 } };
+		int[] moves = { 5, 5, 1, 1, 4 };// 1, 5, 3, 5, 1, 2, 1 };
+		System.out.println(solution(board, moves));
+	}
+
+	// 정확성 : 0점...ㅠ^ㅠ
+	public static int solution(int[][] board, int[] moves) {
+		int answer = 0;
+		List<Integer> stack = new LinkedList<>();
+		int[] indexes = new int[board.length];
+		Arrays.fill(indexes, board.length - 1);
+
+		for (int i = 0; i < moves.length; i++) {
+			int section = moves[i] - 1;
+			int item = indexes[section]--;
+			int doll;
+
+			// 해당 섹션에 더 이상 인형 없을 때
+			if (item < 0)
+				continue;
+
+			doll = board[section][item];
+
+			// 인형이 없을 때
+			if (doll == 0)
+				continue;
+
+			if (stack.size() > 0 && stack.get(stack.size() - 1) == doll) {
+				stack.remove(stack.size() - 1);
+				answer += 2;
+
+			} else
+				stack.add(doll);
+		}
+
+		return answer;
+	}
 }
