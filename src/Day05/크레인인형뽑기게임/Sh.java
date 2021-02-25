093fab869112635cafa211f3614cca5a2b4e2db5
SunghyunKim9744 <dqq9744@naver.com>
2021-02-25 14:19:35 +0900

    성현 : Day05 인형뽑기 추가(큐)

--- src/Day05/크레인인형뽑기게임/Sh.java
+++ src/Day05/크레인인형뽑기게임/Sh.java
@@ -3,7 +3,9 @@ package Day05.크레인인형뽑기게임;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Sh {
 
@@ -48,7 +50,7 @@ public class Sh {
 //    }
 
 	// (0.06ms, 52.1MB) - > (0.77ms, 52.6MB)
-	// 방법 2. 넣을 때 제거
+	// 방법 2. 넣을 때 제거 (인형 제거 X)
 //	public static int solution(int[][] board, int[] moves) {
 //        int answer = 0;
 //        
@@ -73,7 +75,7 @@ public class Sh {
 //    }
 
 	// (0.06ms, 51.7MB) - > (1.19ms, 52.1MB)
-	// 방법 3. 스택 이용
+	// 방법 3. 스택 이용 (인형 제거 X)
 //	public static int solution(int[][] board, int[] moves) {
 //		int answer = 0;
 //
@@ -97,33 +99,71 @@ public class Sh {
 //	}
 	
 	// (0.09ms, 52MB) - > (1.81ms, 52.4MB)
-	// 스택 + 큐
+	// 스택(큐 같은 스택)만 이용 (인형 제거O)
+//	public static int solution(int[][] board, int[] moves) {
+//		int answer = 0;
+//
+//		List<Deque<Integer>> stackList = new ArrayList<>();
+//		for(int i=0; i<board.length; i++) {
+//			Deque<Integer> temp = new ArrayDeque<>();
+//			for(int j=0; j<board.length; j++) {
+//				if(board[j][i] != 0)
+//					temp.offerFirst(board[j][i]);
+//			}
+//			stackList.add(temp);
+//		}
+//
+//		
+//		Deque<Integer> stack = new ArrayDeque<>();
+//		for (int i = 0; i < moves.length; i++) {
+//			int index = moves[i]-1;
+//			if(stackList.get(index).peekLast() == null)
+//				break;
+//			int get = stackList.get(index).pollLast();
+//			if( !stack.isEmpty() && get == stack.peekFirst()) {
+//				stack.pollFirst();
+//				answer+=2;
+//			}
+//			else
+//				stack.offerFirst(get);
+//				
+//		}
+//		return answer;
+//	}
+	
+	// (0.21ms, 51.8MB) - > (2.39ms, 52.7MB)
+	// 큐 + 스택(인형 제거 O)
 	public static int solution(int[][] board, int[] moves) {
 		int answer = 0;
 
-		List<Deque<Integer>> stackList = new ArrayList<>();
+		List<Queue<Integer>> queueList = new ArrayList<>();
 		for(int i=0; i<board.length; i++) {
-			Deque<Integer> temp = new ArrayDeque<>();
+			Queue<Integer> temp = new LinkedList<>();
 			for(int j=0; j<board.length; j++) {
 				if(board[j][i] != 0)
-					temp.offerFirst(board[j][i]);
+					temp.offer(board[j][i]);
 			}
-			stackList.add(temp);
+			queueList.add(temp);
 		}
 
 		
 		Deque<Integer> stack = new ArrayDeque<>();
 		for (int i = 0; i < moves.length; i++) {
 			int index = moves[i]-1;
-			if(stackList.get(index).peekLast() == null)
+			if(queueList.get(index).peek() == null)
 				break;
-			int get = stackList.get(index).pollLast();
+			int get = queueList.get(index).poll();
 			if( !stack.isEmpty() && get == stack.peekFirst()) {
 				stack.pollFirst();
 				answer+=2;
 			}
-			else
+				
+			
+			else 
 				stack.offerFirst(get);
+			
+			
+				
 				
 		}
 		return answer;
