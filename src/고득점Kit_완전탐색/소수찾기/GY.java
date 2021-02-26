7dabada0d128375d18178c2506ebe5ea7f8946e6
aengun <aengun@icloud.com>
2021-02-26 11:40:06 +0900

    건영 : 완전탐색 패키지

---
+++ src/고득점Kit_완전탐색/소수찾기/GY.java
@@ -0,0 +1,14 @@
+package 고득점Kit_완전탐색.소수찾기;
+
+public class GY {
+
+	public static void main(String[] args) {
+
+	}
+
+	public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        return answer;
+    }
+
+}
---
+++ src/고득점Kit_완전탐색/카펫/GY.java
@@ -0,0 +1,14 @@
+package 고득점Kit_완전탐색.카펫;
+
+public class GY {
+
+	public static void main(String[] args) {
+
+	}
+
+	public boolean solution(String[] phone_book) {
+		boolean answer = true;
+		return answer;
+	}
+
+}
