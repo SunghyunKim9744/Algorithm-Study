5964f1442f107a45e7f35a9bebd185c53e9adfeb
Hyun <iamhyunji_kim@icloud.com>
2021-03-03 07:57:48 +0900

    현지 : 베스트 앨범

--- src/고득점Kit/해시/베스트앨범/HJ.java
+++ src/고득점Kit/해시/베스트앨범/HJ.java
@@ -5,9 +5,9 @@ import java.util.*;
 public class HJ {
 
 	public static void main(String[] args) {
-		String[] genres = { "jazz", "classic", "pop", "classic", "classic", "pop" };
-		int[] plays = { 600, 500, 600, 150, 800, 2500 };
-		System.out.println(solution(genres, plays));
+		String[] genres = { "classic", "pop", "classic", "classic", "pop" };
+		int[] plays = { 500, 600, 150, 800, 2500 };
+		System.out.println(Arrays.toString(solution(genres, plays)));
 	}
 
 	public static int[] solution(String[] genres, int[] plays) {
@@ -15,10 +15,41 @@ public class HJ {
 		Map<String, Integer> music = new HashMap<>();
 		List<Integer> album = new ArrayList<>();
 
+		// 장르별 재생 구하기
 		for (int i = 0, len = genres.length; i < len; i++)
 			music.put(genres[i], music.getOrDefault(genres[i], 0) + plays[i]);
 
-		System.out.println(music);
+		// 최대 재생 장르
+		List<String> musicList = new LinkedList<String>(music.keySet());
+		musicList.sort((k1, k2) -> music.get(k2).compareTo(music.get(k1)));
+
+		// 앨범 곡 구하기
+		for (int i = 0, size = musicList.size(); i < size; i++) {
+			String genre = musicList.get(i);
+			List<Integer> temp = new ArrayList<>();
+
+			// 장르별 곡 선정
+			for (int j = 0, len = genres.length; j < len; j++)
+				if (genres[j].equals(genre))
+					temp.add(j);
+
+			// 곡 기준 내림차순 정렬
+			temp.sort((m1, m2) -> {
+				if (m1 == m2)
+					return m1.compareTo(m2);
+				else
+					return plays[m2] - plays[m1];
+			});
+
+			int max = temp.size() < 2 ? temp.size() : 2;
+
+			// 곡 선정
+			for (int j = 0; j < max; j++)
+				album.add(temp.get(j));
+		}
+
+		answer = album.stream().mapToInt(i -> i).toArray();
+
 		return answer;
 	}
 }
