eafdf942a2d1902a2c0db85e349c9bb4c3bb193b
aengun <aengun@icloud.com>
2021-03-04 05:26:25 +0900

    건영 : 큐,스택 문제풀이 완료

--- src/고득점Kit/큐_스택/프린터/GY.java
+++ src/고득점Kit/큐_스택/프린터/GY.java
@@ -1,6 +1,5 @@
 package 고득점Kit.큐_스택.프린터;
 
-import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -16,51 +15,65 @@ public class GY {
 
 	}
 
-	// 9:30 ~
+	// 0.45ms~8.11ms
 	public static int solution(int[] priorities, int location) {
 		int answer = 0;
 
-//		Map<Integer, Integer> map = new HashMap<>();
-//		Queue<Map<Integer, Integer>> queue = new LinkedList<>();
-//		for (int i = 0; i < priorities.length; i++) {
-//			// key : 인덱스, value : 우선순위
-//			map.put(i, priorities[i]);
-//		}
-//		
-//		for(Entry<Integer, Integer> entry : map.entrySet())
-//			queue.offer(entry);
+		List<Prints> list = new LinkedList<>();
+		for (int i = 0; i < priorities.length; i++) {
+			list.add(new Prints(i, priorities[i]));
+		}
 
-		List<Prints> queue = new LinkedList<>();
-		for (int i = 0; i < priorities.length; i++)
-			queue.add(new Prints(i, priorities[i]));
+		while (!list.isEmpty()) {
+			Prints p = list.get(0);
+			
+			boolean isRemoved = false;
+			for (int i = 1; i < list.size(); i++) {
+				if (p.importance < list.get(i).importance) {
+					// 중요도 높은게 있으면 맨 뒤로 보내기
+					list.remove(0);
+					list.add(p);
+					isRemoved = true;
+					break;
+				}
+			}
+			
+			if(isRemoved)
+				continue;
 
-//		Collections.sort(queue);
+			int maxImportance = 0;
+			for (Prints print : list)
+				if (print.importance > maxImportance)
+					maxImportance = print.importance;
 
-		for (Prints p : queue)
-			System.out.println(p.toString());
+			if (list.get(0).location == location)
+				break;
 
-		return answer;
+			if (list.get(0).importance == maxImportance) {
+				list.remove(0);
+				answer++;
+			}
+
+		}
+
+		return answer + 1;
 	}
 
 }
 
-class Prints implements Comparator<Prints> {
-	int order;
+class Prints {
+
+	int location;
 	int importance;
 
-	public Prints(int order, int importance) {
-		this.order = order;
+	public Prints(int location, int importance) {
+		this.location = location;
 		this.importance = importance;
 	}
 
 	@Override
 	public String toString() {
-		return "Prints [order=" + order + ", importance=" + importance + "]";
-	}
-
-	@Override
-	public int compare(Prints o1, Prints o2) {
-		return o1.importance - o2.importance;
+		return "Prints [location=" + location + ", importance=" + importance + "]";
 	}
 
 }
---
+++ src/자료구조/그리디/GY.java
@@ -0,0 +1,9 @@
+package 자료구조.그리디;
+
+public class GY {
+
+	public static void main(String[] args) {
+
+	}
+
+}
