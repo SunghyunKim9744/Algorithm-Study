5895dce1f8b4cef4c7420c3f55ffb48a43708f97
SunghyunKim9744 <dqq9744@naver.com>
2021-03-06 14:10:58 +0900

    성현 : 고득점Kit(큐,스택) 트럭,프린터 다시 풀기

--- src/고득점Kit/큐_스택/다리를지나는트럭/Sh.java
+++ src/고득점Kit/큐_스택/다리를지나는트럭/Sh.java
@@ -13,41 +13,87 @@ public class Sh {
 		System.out.println(solution(b1, w1, t1));
 		System.out.println(solution(b2, w2, t2));
 		System.out.println(solution(b3, w3, t3));
+
 	}
 
 	// 5:04 ~ 6:05
 	// (0.32ms, 52.4MB) - > (56.36ms, 63.1MB)
+//	public static int solution(int bridge_length, int weight, int[] truck_weights) {
+//
+//		int answer = 0;
+//
+//		Queue<Integer> waitQueue = new LinkedList<>();
+//
+//		for (int i = 0; i < truck_weights.length; i++)
+//			waitQueue.offer(truck_weights[i]);
+//
+//		Queue<Integer> finishQueue = new LinkedList<>();
+//		Queue<int[]> goingQueue = new LinkedList<>();
+//
+//		int totalWeight = 0;
+//		while (finishQueue.size() != truck_weights.length) {
+//			for (int[] i : goingQueue)
+//				i[1] += 1;
+//
+//			if (!goingQueue.isEmpty() && goingQueue.peek()[1] == bridge_length) {
+//				int temp = goingQueue.poll()[0];
+//				finishQueue.offer(temp);
+//				totalWeight -= temp;
+//			}
+//			if (!waitQueue.isEmpty() && waitQueue.peek() + totalWeight <= weight) {
+//				int temp = waitQueue.poll();
+//				goingQueue.offer(new int[] {temp,0});
+//				totalWeight += temp;
+//			}
+//			answer++;
+//
+//		}
+//		return answer;
+//	}
+
 	public static int solution(int bridge_length, int weight, int[] truck_weights) {
 
 		int answer = 0;
 
-		Queue<Integer> waitQueue = new LinkedList<>();
-
-		for (int i = 0; i < truck_weights.length; i++)
-			waitQueue.offer(truck_weights[i]);
-
-		Queue<Integer> finishQueue = new LinkedList<>();
-		Queue<int[]> goingQueue = new LinkedList<>();
-
 		int totalWeight = 0;
-		while (finishQueue.size() != truck_weights.length) {
-			for (int[] i : goingQueue)
-				i[1] += 1;
+		Queue<MyTruck> wait = new LinkedList<>();
+		for (int i = 0; i < truck_weights.length; i++)
+			wait.offer(new MyTruck(truck_weights[i], 0));
 
-			if (!goingQueue.isEmpty() && goingQueue.peek()[1] == bridge_length) {
-				int temp = goingQueue.poll()[0];
-				finishQueue.offer(temp);
-				totalWeight -= temp;
+		Queue<MyTruck> going = new LinkedList<>();
+
+		while (!(wait.isEmpty() && going.isEmpty())) {
+			for (MyTruck t : going)
+				t.move();
+
+			if (!going.isEmpty() && going.peek().time == bridge_length) {
+				totalWeight -= going.peek().weight;
+				going.poll();
 			}
-			if (!waitQueue.isEmpty() && waitQueue.peek() + totalWeight <= weight) {
-				int temp = waitQueue.poll();
-				goingQueue.offer(new int[] {temp,0});
-				totalWeight += temp;
+
+			if (!wait.isEmpty() && wait.peek().weight + totalWeight <= weight) {
+				totalWeight += wait.peek().weight;
+				going.offer(wait.poll());
 			}
 			answer++;
 
 		}
+
 		return answer;
 	}
 
+}
+
+class MyTruck {
+	int weight;
+	int time;
+
+	public MyTruck(int weight, int time) {
+		this.weight = weight;
+		this.time = 0;
+	}
+
+	public void move() {
+		this.time++;
+	}
 }
--- src/고득점Kit/큐_스택/프린터/Sh.java
+++ src/고득점Kit/큐_스택/프린터/Sh.java
@@ -1,9 +1,9 @@
 package 고득점Kit.큐_스택.프린터;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Sh {
 
@@ -18,50 +18,85 @@ public class Sh {
 	}
 
 	// (0.08ms, 52.8MB) - > (4.55ms, 52.9MB)
+//	public static int solution(int[] priorities, int location) {
+//		int answer = 0;
+//		
+//		Deque<int[]> bowlDeq = new ArrayDeque<>();
+//		Deque<int[]> answerDeq = new ArrayDeque<>();
+//		List<Integer> list = new ArrayList<>();
+//		
+//		for(int i=0; i<priorities.length; i++) {
+//			list.add(priorities[i]);
+//			bowlDeq.offerLast(new int[] {i,priorities[i]});
+//		}
+//		
+//		
+//		while(!bowlDeq.isEmpty()) {
+//			int[] temp = bowlDeq.pollFirst();
+//			boolean bigger = false;
+//			int index = 0;
+//			for(int i=0; i<list.size()-1; i++)
+//				if(temp[1] < list.get(i+1)) {
+//					bigger = true;
+//					index = i;
+//					break;
+//				}
+//
+//			if(bigger) {
+//				bowlDeq.offerLast(temp);
+//				list.remove(index);
+//				list.add(temp[1]);
+//			}
+//			else {
+//				answerDeq.offerLast(temp);
+//				list.remove(0);
+//			}
+//		}
+//	
+//		int index = 1;
+//		for(int[] arr : answerDeq) {
+//			if(arr[0] == location)
+//				return index;
+//			index++;
+//		}
+//
+//
+//		return answer;
+//	}
+	
 	public static int solution(int[] priorities, int location) {
 		int answer = 0;
-		
-		Deque<int[]> bowlDeq = new ArrayDeque<>();
-		Deque<int[]> answerDeq = new ArrayDeque<>();
+		Queue<Integer> queue = new LinkedList<>();
 		List<Integer> list = new ArrayList<>();
-		
 		for(int i=0; i<priorities.length; i++) {
 			list.add(priorities[i]);
-			bowlDeq.offerLast(new int[] {i,priorities[i]});
+			queue.offer(priorities[i]);
 		}
+			
 		
+		list.sort((i1,i2)->i2.compareTo(i1));
 		
-		while(!bowlDeq.isEmpty()) {
-			int[] temp = bowlDeq.pollFirst();
-			boolean bigger = false;
-			int index = 0;
-			for(int i=0; i<list.size()-1; i++)
-				if(temp[1] < list.get(i+1)) {
-					bigger = true;
-					index = i;
-					break;
-				}
-
-			if(bigger) {
-				bowlDeq.offerLast(temp);
-				list.remove(index);
-				list.add(temp[1]);
-			}
-			else {
-				answerDeq.offerLast(temp);
+		while(true) {
+			int max = list.get(0);
+			int currentPrinter = queue.peek();
+			
+			if(max <= currentPrinter) {
+				answer++;
+				queue.poll();
 				list.remove(0);
+				if(location == 0)
+					return answer;
 			}
+			
+			else {
+				if(location == 0)
+					location = queue.size();
+				queue.offer(queue.poll());
+			}
+			
+			location--;
 		}
-	
-		int index = 1;
-		for(int[] arr : answerDeq) {
-			if(arr[0] == location)
-				return index;
-			index++;
-		}
-
-
-		return answer;
+		
 	}
 
 }
