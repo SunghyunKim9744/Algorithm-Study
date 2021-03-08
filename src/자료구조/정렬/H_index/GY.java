bccc61f2bea86d68deececc0c171858a04af1e3d
aengun <aengun@icloud.com>
2021-03-08 22:48:48 +0900

    건영 : 정렬 패키지 커밋

---
+++ src/자료구조/정렬/H_index/GY.java
@@ -0,0 +1,16 @@
+package 자료구조.정렬.H_index;
+
+public class GY {
+
+	public static void main(String[] args) {
+		int[] citations = { 3, 0, 6, 1, 5 };
+		System.out.println(solution(citations));
+	}
+
+	public static int solution(int[] citations) {
+		int answer = 0;
+
+		return answer;
+	}
+
+}
---
+++ src/자료구조/정렬/가장큰수/GY.java
@@ -0,0 +1,18 @@
+package 자료구조.정렬.가장큰수;
+
+public class GY {
+
+	public static void main(String[] args) {
+		int[] numbers1 = { 6, 10, 2 }, numbers2 = { 3, 30, 34, 5, 9 };
+
+		System.out.println(solution(numbers1));
+		System.out.println(solution(numbers2));
+	}
+
+	public static String solution(int[] numbers) {
+		String answer = "";
+
+		return answer;
+	}
+
+}
