b315f62af02f0dcb8981280bd7e946d52778776f
kdyddd <kdyddd@gmail.com>
2021-03-14 12:00:19 +0900

    동영: 문제클래스 추가, 타겟넘버 완료

--- src/고득점Kit/깊이_너비_우선탐색/타겟넘버/Dy.java
+++ src/고득점Kit/깊이_너비_우선탐색/타겟넘버/Dy.java
@@ -1,14 +1,32 @@
 package 고득점Kit.깊이_너비_우선탐색.타겟넘버;
 
+// 11:06 ~ 11:57
 public class Dy {
 	public static void main(String[] args) {
-		int[] numbers = {1,1,1,1,1};
+		int[] numbers = {1,1,1};
 		int target = 3;
 		System.out.println(solution(numbers, target));
 	}
 
 	public static int solution(int[] numbers, int target) {
         int answer = 0;
+        answer = dfs(numbers, target, 0);
         return answer;
     }
+	
+	public static int dfs(int[] numbers, int target, int deps) {
+		int num = 0;
+		if(deps == numbers.length) {
+			if(target == 0) {
+				return 1;
+			}else {
+				return 0;
+			}
+		}
+		int i = deps++;
+		num += dfs(numbers, target-numbers[deps], i);
+		num += dfs(numbers, target+numbers[deps], i);
+		
+		return num;
+	}
 }
---
+++ src/고득점Kit/깊이_너비_우선탐색/타겟넘버/문제.java
@@ -0,0 +1,15 @@
+package 고득점Kit.깊이_너비_우선탐색.타겟넘버;
+
+public class 문제 {
+	public static void main(String[] args) {
+		int[] numbers = {1,1,1};
+		int target = 3;
+		System.out.println(solution(numbers, target));
+	}
+
+	public static int solution(int[] numbers, int target) {
+        int answer = 0;
+        return answer;
+    }
+	
+}
