3f721c4aed8f7ec421eef8903b6cb2059f1cff8d
kdyddd <kdyddd@gmail.com>
2021-03-14 11:05:49 +0900

    동영: 고득점kit 깊이 너비 우선탐색 패키지 생성

---
+++ src/고득점Kit/깊이_너비_우선탐색/네트워크/Dy.java
@@ -0,0 +1,14 @@
+package 고득점Kit.깊이_너비_우선탐색.네트워크;
+
+public class Dy {
+	public static void main(String[] args) {
+		int n = 3;
+		int[][] computers = {{1,1,0},{1,1,0},{0,0,1}};
+		System.out.println(solution(n, computers));
+	}
+
+	public static int solution(int n, int[][] computers) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ src/고득점Kit/깊이_너비_우선탐색/단어변환/Dy.java
@@ -0,0 +1,14 @@
+package 고득점Kit.깊이_너비_우선탐색.단어변환;
+
+public class Dy {
+	public static void main(String[] args) {
+		String[][] tickets = {{"ICN", "JFK"},{"HND","IAD"},{"JFK","HND"}};
+		
+		System.out.println(solution(tickets));
+	}
+
+	public static String[] solution(String[][] tickets) {
+        String[] answer = {};
+        return answer;
+    }
+}
