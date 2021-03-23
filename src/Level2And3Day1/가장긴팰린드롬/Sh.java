51ccc5402229e71ccbb74c29018feb7e71b4589c
SunghyunKim9744 <dqq9744@naver.com>
2021-03-24 00:52:29 +0900

    성현 : 가장 긴 팰른드롬(효율성2번 ㅠ)

--- src/Level2And3Day1/가장긴팰린드롬/Sh.java
+++ src/Level2And3Day1/가장긴팰린드롬/Sh.java
@@ -1,10 +1,14 @@
 package Level2And3Day1.가장긴팰린드롬;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Sh {
 
 	public static void main(String[] args) {
 		
-		String s = "abcdcba";
+		String s = "ecdabbeadc";
 		
 		System.out.println(solution(s));
 
@@ -13,7 +17,44 @@ public class Sh {
 	private static int solution(String s) {
 		int answer = 0;
 		
-		return answer;
+		String reverse = new StringBuilder(s).reverse().toString();
+		Map<String,Integer[]> map = new HashMap<>();
+		
+		int max = 1;
+		int len = s.length();
+		for(int i=0; i<s.length(); i++) {
+			StringBuilder temp = new StringBuilder(s.substring(i,i+1));
+			for(int j=i+1; j<s.length(); j++) {
+				temp = temp.append(s.substring(j,j+1));
+				if(reverse.contains(temp)) {
+					//if(max<temp.length()) {
+					//	max = temp.length();
+						map.put(temp.toString(), new Integer[] {i,j});
+					//}
+						
+				}
+				else {
+					break;
+				}
+					
+			}
+		}
+		
+		Object[] key = map.keySet().toArray();
+		
+		Arrays.sort(key,(o1,o2)->((String)o2).length()-((String)o1).length());
+		
+		for(Object ob : key) {
+			String s1 = (String)ob;
+			Integer[] temp = map.get(s1);
+			int begin = len-temp[1]-1;
+			int end = len-temp[0];
+			String reverseStr = reverse.substring(begin,end);
+			if(reverseStr.equals(s1)) {
+				return s1.length();
+			}
+		}
+		return max;
 		
 	}
 
