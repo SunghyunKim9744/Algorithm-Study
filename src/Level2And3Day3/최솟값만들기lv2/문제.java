d7d82a4323fe33fdfe603a6d8913c53a94df7ad0
aengun <aengun@icloud.com>
2021-03-27 16:01:26 +0900

    건영 : 패키지

---
+++ src/Level2And3Day3/최솟값만들기lv2/문제.java
@@ -0,0 +1,20 @@
+package Level2And3Day3.최솟값만들기lv2;
+
+public class 문제 {
+
+	public static void main(String[] args) {
+		int[] A1 = { 1, 4, 2 }, B1 = { 5, 4, 4 };
+		int[] A2 = { 1, 2 }, B2 = { 3, 4 };
+
+		System.out.println(solution(A1, B1));
+		System.out.println(solution(A2, B2));
+
+	}
+
+	public static int solution(int[] A, int[] B) {
+		int answer = 0;
+
+		return answer;
+	}
+
+}
---
+++ src/Level2And3Day3/피보나치수lv2/문제.java
@@ -0,0 +1,20 @@
+package Level2And3Day3.피보나치수lv2;
+
+public class 문제 {
+
+	public static void main(String[] args) {
+
+		int n1 = 3, n2 = 5;
+
+		System.out.println(solution(n1));
+		System.out.println(solution(n2));
+
+	}
+
+	public static int solution(int n) {
+		int answer = 0;
+
+		return answer;
+	}
+
+}
