22a07ffdaa972e319ece25b03dcf3c46c9b6ee5a
aengun <aengun@icloud.com>
2021-03-29 03:24:22 +0900

    건영 : 가장 긴 팰린드롬, 땅따먹기 => 구글링

--- src/Level2And3Day2/땅따먹기lv2/GY.java
+++ src/Level2And3Day2/땅따먹기lv2/GY.java
@@ -9,26 +9,22 @@ public class GY {
 
 	}
 
+	// 구글링
 	private static int solution(int[][] land) {
 
 		int answer = 0;
+		for (int i = 1; i < land.length; i++) {
 
-		int index = 0;
-		
-		for (int i = 0; i < land[0].length; i++) {
-			
-			index = i;
-			int sum = 0;
-			
-			for (int j = 0; j < land.length; j++) {
+			land[i][0] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][3]));
+			land[i][1] += Math.max(land[i - 1][0], Math.max(land[i - 1][2], land[i - 1][3]));
+			land[i][2] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][3]));
+			land[i][3] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][2]));
 
-			}
-			
-			if (answer < sum)
-				answer = sum;
-		
 		}
 
+		for (int i = 0; i < land[0].length; i++)
+			answer = (answer < land[land.length-1][i]) ? land[land.length-1][i] : answer;
+		
 		return answer;
 
 	}
