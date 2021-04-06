abd62af7c178bdf9e805e4c3dad20d39093604a4
SunghyunKim9744 <dqq9744@naver.com>
2021-04-06 18:32:30 +0900

    성현 : 불량사용자 통과

--- src/KaKaO2019WinterIntern/불량사용자/Sh.java
+++ src/KaKaO2019WinterIntern/불량사용자/Sh.java
@@ -11,68 +11,59 @@ import java.util.Set;
 public class Sh {
 
 	public static void main(String[] args) {
-		String[] user_id = {"frodo", "fradi", "crodo", "abc123", "frodoc"};
-		String[] banned_id = {"fr*d*", "*rodo","******", "******"};
-		
-		int answer = solution(user_id,banned_id);
+		String[] user_id = { "frodo", "fradi", "crodo", "abc123", "frodoc" };
+		String[] banned_id = { "fr*d*", "*rodo", "******", "******" };
+
+		int answer = solution(user_id, banned_id);
 		System.out.println(answer);
 	}
 
 	private static int solution(String[] user_id, String[] banned_id) {
 		int answer = 0;
-		
-		Map<Integer,List<String>> map = new HashMap<>();
-		for(int i=0; i<banned_id.length; i++) {
+
+		Map<Integer, List<String>> map = new HashMap<>();
+		for (int i = 0; i < banned_id.length; i++) {
 			String ban = banned_id[i];
 			List<String> list = new ArrayList<>();
-			for(int j=0; j<user_id.length; j++) {
+			for (int j = 0; j < user_id.length; j++) {
 				String user = user_id[j];
 				boolean bool = true;
-				if(user.length() != ban.length())
+				if (user.length() != ban.length())
 					continue;
-				for(int k=0; k<user.length(); k++) {
+				for (int k = 0; k < user.length(); k++) {
 					char userCh = user.charAt(k);
 					char banCh = ban.charAt(k);
-					if(banCh == '*')
+					if (banCh == '*')
 						continue;
-					else if(userCh != banCh) {
+					else if (userCh != banCh) {
 						bool = false;
 						break;
-					}	
+					}
 				}
-				if(bool)
+				if (bool)
 					list.add(user);
 			}
-			if(!list.isEmpty())
-				map.put(i,list);
+			if (!list.isEmpty())
+				map.put(i, list);
 		}
-		//System.out.println(map);
+		// System.out.println(map);
 
 		List<String> list = new ArrayList<>();
 		String[] temp = new String[map.size()];
 		String str = "";
 		int depth = 0;
-		dfs(map,temp,str,depth,list);
+		dfs(map, temp, str, depth, list);
+	
+		Set<String> set1 = new HashSet<>(list);
 		
-		Set<String> set1 = new HashSet<>();
-		for(String s : list) {
-			Set<String> strSet = new HashSet<>();
-			String[] tempS = s.split(" ");
-			for(String s1 : tempS) {
-				strSet.add(s1);
-			}
-			if(strSet.size() == map.size())
-				set1.add(s);
-				
-		}
-		
+
 		Set<String> answers = new HashSet<>();
-		for(String tempString : set1) {
+		for (String tempString : set1) {
 			String[] temps = tempString.split(" ");
 			Arrays.sort(temps);
 			String tempS1 = "";
-			for(String te : temps)
-				tempS1+=te;
+			for (String te : temps)
+				tempS1 += te;
 			answers.add(tempS1);
 		}
 		answer = answers.size();
@@ -80,22 +71,31 @@ public class Sh {
 	}
 
 	private static void dfs(Map<Integer, List<String>> map, String[] temp, String str, int depth, List<String> list) {
-		if(depth == map.size()) {
+		if (depth == map.size()) {
 			String s = "";
-			for(String str1 : temp)
+			for (String str1 : temp)
 				s += str1 + " ";
 			list.add(s);
 			return;
 		}
-//		for(int i=0; i<depth; i++) {
-//			if(temp[i].equals(str))
-//				return;
-//		}
-		for(int i=0; i<map.get(depth).size(); i++) {
-			temp[depth] = map.get(depth).get(i);
-			dfs(map, temp, temp[depth], depth+1, list);
-		}
 		
+		
+		for (int i = 0; i < map.get(depth).size(); i++) {
+			str = map.get(depth).get(i);
+			boolean bool = true;
+			for(int j=0; j<depth; j++) {
+				if(temp[j].equals(str)) {
+					bool=false;
+					break;
+				}
+			}
+			if(bool) {
+				temp[depth] = str;
+				dfs(map, temp, str, depth + 1, list);
+			}
+			
+		}
+
 	}
 
 }
