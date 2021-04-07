51b31967a4bd8cdebb2da222c745b9c0ba2173af
SunghyunKim9744 <dqq9744@naver.com>
2021-04-07 17:07:01 +0900

    성현 : 징검다리

--- src/KaKaO2019WinterIntern/징검다리건너기/Sh.java
+++ src/KaKaO2019WinterIntern/징검다리건너기/Sh.java
@@ -3,8 +3,56 @@ package KaKaO2019WinterIntern.징검다리건너기;
 public class Sh {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] stones = { 8 };
+		int k = 3;
 
+		int result = solution(stones, k);
+
+		System.out.println(result);
+
+	}
+
+	private static int solution(int[] stones, int k) {
+		int answer = 0;
+
+		int start = 0;
+		int end = 0;
+
+		for (Integer i : stones) {
+			if (end < i)
+				end = i;
+		}
+
+		while (start <= end) {
+			int mid = (start + end) / 2;
+
+			int cnt = 0;
+			int temp = 0;
+			for (Integer i : stones) {
+				int value = i - mid;
+				// =
+				if (value < 0) {
+					temp++;
+					//
+					if (temp > cnt)
+						cnt = temp;
+				} else {
+					if (temp > cnt)
+						cnt = temp;
+					temp = 0;
+				}
+			}
+			if (cnt < k) {
+				answer = mid;
+				start = mid + 1;
+			}
+
+			else {
+				end = mid - 1;
+			}
+
+		}
+		return answer;
 	}
 
 }
--- src/KaKaO2019WinterIntern/호텔방배정/Sh.java
+++ src/KaKaO2019WinterIntern/호텔방배정/Sh.java
@@ -1,10 +1,74 @@
 package KaKaO2019WinterIntern.호텔방배정;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Sh {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		long k = 10;
+		long[] room_number = {1,3,4,1,3,1};
+		
+		long[] answer = solution(k,room_number);
 
 	}
 
+//	private static long[] solution(long k, long[] room_number) {
+//		long[] answer = new long[room_number.length];
+//		Set<Long> use_room = new HashSet<>();
+//		int index = 0;
+//		for(long l : room_number) {
+//			if(!use_room.contains(l)) {
+//				answer[index++] = l;
+//				use_room.add(l);
+//			}
+//			else {
+//				for(long i = l; i<=k; i++) {
+//					if(!use_room.contains(i)) {
+//						answer[index++] = i;
+//						use_room.add(i);
+//						break;
+//					}
+//				}
+//			}
+//			
+//		}
+//		
+//		
+//		return answer;
+//	}
+
+	private static long[] solution(long k, long[] room_number) {
+		long[] answer = new long[room_number.length];
+		Map<Long,Long> map = new HashMap<>();
+		
+		int index = 0;
+		for(Long l : room_number) {
+			if(!map.containsKey(l)) {
+				map.put(l,(long) 1);
+				answer[index++] = l;
+			}
+			else {
+				long min = 0;
+				for(Long l2 : map.keySet()) {
+					if(l2 <=l) {
+						min+= map.get(l2);
+					}
+				}
+				map.put(l, map.get(l)+1);
+				for(long i = min; min<=k; i++) {
+					if(!map.containsKey(i)) {
+						map.put(i, (long) 1);
+						answer[index++] = i;
+						break;
+					}
+				}
+			}
+		}
+		
+//		for(long l : answer)
+//			System.out.println(l);
+		return answer;
+	}
+	
 }
