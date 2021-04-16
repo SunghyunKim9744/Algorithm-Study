3a1660dac4bab95ddd9e01c22ff980e0a5fdd18f
SunghyunKim9744 <dqq9744@naver.com>
2021-04-16 18:34:21 +0900

    성현 : 2018 - 2019 카카오

--- src/KaKaO20183차/n진수게임/Sh.java
+++ src/KaKaO20183차/n진수게임/Sh.java
@@ -6,5 +6,41 @@ public class Sh {
 		// TODO Auto-generated method stub
 
 	}
+	
+	// 2:22
+	public String solution(int n, int t, int m, int p) {
+        String answer = "";
+        String[] answers = new String[m*t+1];
+        answers[1] = "0";
+        int i=2;
+        int num=1;
+        while(i<m*t+1){
+            StringBuilder strb = new StringBuilder();
+            int tempNum = num;
+            while(tempNum > 0){
+                int temp = tempNum % n;
+                if(temp >= 10){
+                    int temp2 = (temp-10) +'A';
+                    char temp3 = (char)temp2;
+                    strb.insert(0,temp3);
+                }
+                else{
+                    strb.insert(0,temp);
+                }
+                tempNum /= n;
+                
+            }
+            for(int j=0; j<strb.length(); j++){
+                if(i == m*t+1)
+                    break;
+                answers[i++] = strb.toString().substring(j,j+1);
+            }
+            num++;
+        }
+        
+         for(int j=0; j<t; j++)
+             answer += answers[j*m+p];
+        return answer;
+    }
 
 }
--- src/KaKaO2019신입/무지의먹방라이브/Sh.java
+++ src/KaKaO2019신입/무지의먹방라이브/Sh.java
@@ -1,10 +1,74 @@
 package KaKaO2019신입.무지의먹방라이브;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 public class Sh {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] food_times = {3,3,2};
+		long k = 10;
+		
+		int answer = solution(food_times,k);
 
+	}
+
+	private static int solution(int[] food_times, long k) {
+		int answer = 0;
+		Set<Integer> set = new LinkedHashSet<>();
+		Map<Integer,Integer> map = new LinkedHashMap<>();
+		int cnt=1;
+		for(int i : food_times)
+			map.put(cnt++,i);
+		Arrays.sort(food_times);
+		for(int i : food_times)
+			set.add(i);
+		List<Integer> list = new ArrayList<>(set);
+		k--;
+		int temp = 0;
+		while(k != 0) {
+			System.out.println("몇번");
+			System.out.println(k);
+			long min = (long)list.get(0)-temp;
+			int len = map.size();
+			if(min*len < k) {
+				k-=min*len;
+				temp = list.get(0);
+				list.remove(0);
+				Iterator<Entry<Integer, Integer>>iter = map.entrySet().iterator();
+				while (iter.hasNext()) {
+					if(iter.next().getValue() == temp)
+						iter.remove();
+				}
+			}
+			if(min*len >=k) {
+				System.out.println("hi");
+				System.out.println(k);
+				int turn = (int) (k%map.size()+1);
+				System.out.println(turn);
+				
+				int index = 0;
+				for(Integer i : map.keySet()) {
+					if(index == turn) {
+						answer = i;
+						k=0;
+						break;
+					}
+					index++;
+						
+				}
+			}
+			
+		}
+		System.out.println(answer);
+		return answer;
 	}
 
 }
