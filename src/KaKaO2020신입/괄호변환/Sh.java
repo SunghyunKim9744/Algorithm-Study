462ea078c7800aaf2d2a3c1670a93e3566d510b6
SunghyunKim9744 <dqq9744@naver.com>
2021-04-20 15:38:20 +0900

    성현 : 2020카카오

--- src/KaKaO2020신입/괄호변환/Sh.java
+++ src/KaKaO2020신입/괄호변환/Sh.java
@@ -3,8 +3,98 @@ package KaKaO2020신입.괄호변환;
 public class Sh {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String p = "()))((";
+		
+		String answer = solution(p);
 
+	}
+
+	private static String solution(String p) {
+		String answer = "";
+		
+		answer = recu(p);
+		System.out.println(answer);
+		return answer;
+	}
+
+	private static String recu(String p) {
+		
+		if(p.isEmpty())
+			return "";
+		
+		String[] strs = separation(p);
+		String u = strs[0];
+		
+		
+		String v = strs[1];
+		
+		
+		if(checkU(u)) {	// 올바른 문자열
+			System.out.println("올바른 u : " + u);
+			u = u+recu(v);
+			
+			return u;
+		}
+		else {			// 올바른 문자열 X
+			System.out.println("올바르지않은 u : " + u);
+			String temp = "(" + recu(v) +")";
+			
+			String s = "";
+			
+			if(u.length()>2) {
+				u = u.substring(1,u.length()-1);
+				
+				for(int i=0; i<u.length(); i++) {
+					String t = u.substring(i,i+1);
+					if(t.equals("("))
+						s+=")";
+					else
+						s+="(";
+				}
+			}
+			
+			return temp+s;
+			
+		}
+		
+	}
+
+	private static boolean checkU(String u) {
+		int cnt = 0;
+		for(int i=0; i<u.length(); i++) {
+			char ch = u.charAt(i);
+			if(ch=='(')
+				cnt++;
+			else
+				cnt--;
+			if(cnt < 0)
+				return false;
+		}
+		return true;
+	}
+
+	private static String[] separation(String p) {
+		int left = 0;
+		int right = 0;
+		String[] strs = new String[2];
+		for(int i=0; i<p.length(); i++) {
+			char ch = p.charAt(i);
+			if(ch=='(')
+				left++;
+			else
+				right++;
+			if(left == right) {
+				String u = p.substring(0,i+1);
+				String v = "";
+				if(i+1 < p.length())
+					v = p.substring(i+1);
+				
+				strs[0] = u;
+				strs[1] = v;
+				break;
+			}
+		}
+		return strs;
 	}
 
 }
--- src/KaKaO2020신입/문자열압축/Sh.java
+++ src/KaKaO2020신입/문자열압축/Sh.java
@@ -3,8 +3,57 @@ package KaKaO2020신입.문자열압축;
 public class Sh {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String s = "xababcdcdababcdcd";
+		
+		int answer = solution(s);
 
+	}
+
+	private static int solution(String s) {
+		int answer = 0;
+		int max = s.length();
+		
+		int maxCut = s.length()/2;
+		
+		for(int i=1; i<=maxCut; i++) {
+			int num = s.length()/i;
+			int last = s.length()%i;
+			StringBuilder strb = new StringBuilder();
+			
+			String temp = s.substring(0,i);
+			int cnt = 1;
+			for(int j=1; j<num; j++) {
+				String current = s.substring(j*i,j*i+i);
+				if(current.equals(temp)) {
+					cnt++;
+				}
+				else {
+					if(cnt != 1) 
+						strb.append(cnt+temp);
+					else 
+						strb.append(temp);
+						
+					temp = current;
+					cnt = 1;
+				}
+				
+				if(j == num-1) {
+					if(cnt != 1) 
+						strb.append(cnt+temp);
+					else 
+						strb.append(temp);
+				}
+					
+			}
+			
+			int len = strb.length() + last;
+			if(max > len) 
+				max = len;
+			
+		}
+		answer = max;
+		System.out.println(answer);
+		return answer;
 	}
 
 }
