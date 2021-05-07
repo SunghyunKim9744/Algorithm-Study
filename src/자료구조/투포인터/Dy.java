20619f49980a9c9046b7ac519f7a0f2a5284fdb5
SunghyunKim9744 <dqq9744@naver.com>
2021-05-07 10:08:55 +0900

    성현 : 투포인터

---
+++ src/자료구조/투포인터/Dy.java
@@ -0,0 +1,12 @@
+package 자료구조.투포인터;
+
+public class Dy {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,2,5,3,1,1,2};
+		
+		int total = 5;
+
+	}
+
+}
---
+++ src/자료구조/투포인터/Sh.java
@@ -0,0 +1,12 @@
+package 자료구조.투포인터;
+
+public class Sh {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,2,5,3,1,1,2};
+		
+		int total = 5;
+
+	}
+
+}
