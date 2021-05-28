d78e614d7fe57357842f1cdb5f2ea54c173272ea
SunghyunKim9744 <dqq9744@naver.com>
2021-05-28 17:04:13 +0900

    성현 : 단체사진, 카카오프렌즈

--- src/Level2Day01/단체사진찍기/Sh.java
+++ src/Level2Day01/단체사진찍기/Sh.java
@@ -1,10 +1,94 @@
 package Level2Day01.단체사진찍기;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Sh {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int n = 2;
+		String[] data = {"N~F=0", "R~T>2"};
+		
+		int answer = solution(n,data);
 
+	}
+
+	private static int solution(int n, String[] data) {
+		int answer = 0;
+		String[] arr = {"A","C","F","J","M","N","R","T"};
+		boolean[] visited = new boolean[8];
+		List<String> list = new ArrayList<>();
+		String[] temp = new String[8];
+		int depth = 0;
+		int r = 8;
+		permutation(arr,temp,visited,depth,r,list);
+		
+//		Arrays.sort(data,(s1,s2)->{
+//			int n1 = Integer.parseInt(s1.substring(4,5));
+//			int n2 = Integer.parseInt(s2.substring(4,5));
+//			
+//			return n2-n1;
+//		});
+		int cnt = 0;
+		for(int i=0; i<list.size(); i++) {
+			boolean bool = false;
+			
+			for(String s : data) {
+				String people1 = s.substring(0,1);
+				String people2 = s.substring(2,3);
+				String comp = s.substring(3,4);
+				int value = Integer.parseInt(s.substring(4,5));
+				String str = list.get(i);
+				
+				int index1 = str.indexOf(people1);
+				int index2 = str.indexOf(people2);
+				
+				int betweenNum = Math.abs(index1-index2)-1;
+				
+				if(comp.equals("=") && (value != betweenNum) ) {
+					bool = true;
+					break;
+				}
+				
+				else if(comp.equals(">") && (value >= betweenNum)) {
+					bool = true;
+					break;
+				}
+				
+				else if(comp.equals("<") && (value <= betweenNum)){
+					bool = true;
+					break;
+				}
+			}
+			if(!bool)
+				cnt++;
+		}
+		
+		answer = cnt;
+		System.out.println(answer);
+		return answer;
+	}
+
+	private static void permutation(String[] arr, String[] temp, boolean[] visited, int depth, int r, List<String> list) {
+		
+		if(depth == r) {
+			StringBuilder sb = new StringBuilder();
+			for(String s : temp)
+				sb.append(s);
+			
+			list.add(sb.toString());
+			return;
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				temp[depth] = arr[i];
+				permutation(arr, temp, visited, depth+1, r, list);
+				visited[i] = false;
+			}
+		}
 	}
 
 }
--- src/Level2Day01/카카오프렌즈컬러링북/Sh.java
+++ src/Level2Day01/카카오프렌즈컬러링북/Sh.java
@@ -1,10 +1,75 @@
 package Level2Day01.카카오프렌즈컬러링북;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Sh {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int m = 6;
+		int n = 4;
+		
+		int[][] picture = {{1, 1, 1, 0}, {1, 2, 2, 0}, {1, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 3}, {0, 0, 0, 3}};
+		
+		int[] answer = solution(m,n,picture);
 
+	}
+
+	private static int[] solution(int m, int n, int[][] picture) {
+		int numberOfArea = 0;
+        int maxSizeOfOneArea = 0;
+
+        int[] answer = new int[2];
+        answer[0] = numberOfArea;
+        answer[1] = maxSizeOfOneArea;
+        
+		boolean[][] visited = new boolean[m][n];
+		for(int x=0; x<m; x++) {
+			for(int y=0; y<n; y++) {
+				if( (picture[x][y] !=0) && (!visited[x][y]) ) {
+					numberOfArea++;
+					int cnt = bfs(picture,x,y,visited);
+					maxSizeOfOneArea = Math.max(cnt, maxSizeOfOneArea);
+				}
+			}
+		}
+		
+		answer[0] = numberOfArea;
+		answer[1] = maxSizeOfOneArea;
+        return answer;
+	}
+
+	private static int bfs(int[][] picture, int x, int y, boolean[][] visited) {
+		
+		int[] dx = {1,-1,0,0}; // 동 서
+		int[] dy = {0,0,-1,1}; // 남 북
+		
+		Queue<int[]> queue = new LinkedList<>();
+		visited[x][y] = true;
+		queue.add(new int[] {x,y});
+		int cnt = 1;
+		
+		int m = picture.length;
+		int n = picture[0].length;
+		
+		int value = picture[x][y];
+		while(!queue.isEmpty()) {
+			
+			int[] temp = queue.poll();
+			x = temp[0];
+			y = temp[1];
+			
+			for(int i=0; i<4; i++) {
+				int nextX = x+dx[i];
+				int nextY = y+dy[i];
+				if(nextX < 0 || nextX >= m || nextY < 0 || nextY >= n || visited[nextX][nextY] || picture[nextX][nextY] != value)
+					continue;
+				cnt++;
+				queue.add(new int[] {nextX,nextY});
+				visited[nextX][nextY] = true;
+			}
+		}
+		return cnt;
 	}
 
 }
