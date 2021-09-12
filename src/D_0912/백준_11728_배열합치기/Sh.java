5e61559de6b723b85ca547daa3a51dd58616609e
SunghyunKim9744 <dqq9744@naver.com>
2021-09-12 20:29:25 +0900

    성현 : 일환추가

--- src/D_0912/백준_11728_배열합치기/Sh.java
+++ src/D_0912/백준_11728_배열합치기/Sh.java
@@ -1,5 +1,11 @@
 package D_0912.백준_11728_배열합치기;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 /*
  * 
  * 		https://www.acmicpc.net/problem/11728
@@ -8,8 +14,32 @@ package D_0912.백준_11728_배열합치기;
 
 public class Sh {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int sizeA = Integer.parseInt(st.nextToken());
+		int sizeB = Integer.parseInt(st.nextToken());
+		
+		int[] answer = new int[sizeA+sizeB];
+		
+		int idx = 0;
+		st = new StringTokenizer(br.readLine());
+		while(st.hasMoreTokens())
+			answer[idx++] = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		while(st.hasMoreTokens())
+			answer[idx++] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(answer);
+		
+		StringBuilder sb = new StringBuilder();
+		for(int num : answer)
+			sb.append(num+" ");
+		
+		System.out.println(sb.toString());
 
 	}
 
---
+++ src/D_0912/백준_1965_상자넣기/Ih.java
@@ -0,0 +1,16 @@
+package D_0912.백준_1965_상자넣기;
+
+/*
+ * 
+ * 		https://www.acmicpc.net/problem/1965
+ * 
+ */
+
+public class Ih {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
