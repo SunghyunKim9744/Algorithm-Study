c6a1e498221eeabf867e781725b58bd415d523af
장다빈 <jdb4497@nate.com>
2021-10-11 22:14:56 +0900

    gg

--- src/D_1011/백준_13913_숨바꼭질/Db.java
+++ src/D_1011/백준_13913_숨바꼭질/Db.java
@@ -1,10 +1,66 @@
 package D_1011.백준_13913_숨바꼭질;
 
-public class Db {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
 
+public class Db {
+	public final static int MAX = 200001;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int[] dist = new int[MAX];
+		int[] track = new int[MAX];
+		
+		for(int i=0;i<MAX;i++) {
+			dist[i] = -1;
+			track[i] = -1;
+		}
+		
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(n);
+		dist[n] = 0;
+		while(!q.isEmpty()) {
+			
+			int cur = q.poll();
+			
+			int next = cur-1;
+			go(q,dist,track,cur,next);
+			
+			next = cur+1;
+			go(q,dist,track,cur,next);
+				
+			next = cur*2;
+			go(q,dist,track,cur,next);
+		}
+		System.out.println(dist[m]);
+		
+		Stack<Integer> stack = new Stack<>();
+		
+		int cur=m;
+		while(cur!=-1) {
+			stack.add(cur);
+			cur=track[cur];
+		}
+		
+		while(!stack.isEmpty()) {
+			System.out.print(stack.pop()+" ");
+		}
 	}
-
+	public static boolean isRange(int x) {
+		if(x<0||x>=MAX) return false;
+		return true;
+	}
+	public static void go(Queue<Integer> q,int[] dist,int[] track,int cur,int next) {
+		if(isRange(next) && dist[next]==-1) {
+			dist[next] = dist[cur]+1;
+			track[next] = cur;
+			q.add(next);
+		}
+	}
 }
