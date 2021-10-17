20379659c34bdb1ea24c7019ad51bf954b55b219
kdyddd <kdyddd@gmail.com>
2021-10-17 23:18:40 +0900

    동영 : 1017

--- src/D_1017/백준_2151_거울설치/Dy.java
+++ src/D_1017/백준_2151_거울설치/Dy.java
@@ -1,5 +1,10 @@
 package D_1017.백준_2151_거울설치;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+
 /*
  * 
  * 
@@ -9,6 +14,62 @@ package D_1017.백준_2151_거울설치;
 public class Dy {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(Dy.class.getResourceAsStream("input.txt"));
+		
+		int N = Integer.parseInt(sc.nextLine());
+		
+		String[][] board = new String[N][N];
+		
+		int[][] point = new int[2][2];
+		int point2 = 0;
+		Set<String> set = new HashSet<>();
+		int direction = 0;
+		for(int i=0; i<N; i++) {
+			board[i] = sc.nextLine().split("");
+			for(int j=0; j<N; j++) {
+				if(board[i][j].equals("#")) {
+					point[point2][0] = i;
+					point[point2][1] = j;
+					point2++;
+					if(point2 == 0) {
+						if(i == 0) {
+							direction = 1;
+						}
+						if(j == 0) {
+							direction = 3;
+						}
+						if(j == N - 1) {
+							direction = 2;
+						}
+					}
+				}
+				if(board[i][j].equals("!")) {
+					StringBuilder sb = new StringBuilder();
+					sb.append(i);
+					sb.append(",");
+					sb.append(j);
+					set.add(sb.toString());
+				}
+			}
+		}
+		
+		solution(board, point[0][1], point[0][0], point[1][1], point[1][0], set, 0);
+		
+	}
 
+	private static int solution(String[][] board, int x1, int y1, int x2, int y2, int direction, Set<String> set, int cnt) {
+		int answer = 9999999;
+		
+		int temp = 0;
+		
+		if(direction == 0 && x1 > 0) {
+			temp = solution(board, x1, y1, x2, y2, direction, set, cnt);
+		}
+		
+		
+		
+		
+		
+		return answer;
 	}
 }
--- src/D_1017/백준_2217_로프/Dy.java
+++ src/D_1017/백준_2217_로프/Dy.java
@@ -1,5 +1,11 @@
 package D_1017.백준_2217_로프;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+
 /*
  * 
  * 
@@ -10,6 +16,25 @@ package D_1017.백준_2217_로프;
 public class Dy {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(Dy.class.getResourceAsStream("input.txt"));
+		
+		int N = Integer.parseInt(sc.nextLine());
+		
+		List<Integer> list = new ArrayList<>();
+		
+		for(int i=0; i<N; i++) {
+			list.add(Integer.parseInt(sc.nextLine()));
+		}
+		
+		Collections.sort(list, Collections.reverseOrder());
+		
+		int max = 0;
+		for(int i=0; i<N; i++) {
+			if(list.get(i) * (i+1) > max) {
+				max = list.get(i) * (i+1);
+			}
+		}
+		
+		System.out.println(max);
 	}
 }
