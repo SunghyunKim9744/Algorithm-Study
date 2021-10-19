348fba4d516f4113a32971fd5387b3c0b06397d3
SunghyunKim9744 <dqq9744@naver.com>
2021-10-20 08:59:46 +0900

    성현 : 백준 거울설치 bfs

--- src/D_1017/백준_2151_거울설치/Sh.java
+++ src/D_1017/백준_2151_거울설치/Sh.java
@@ -16,12 +16,12 @@ public class Sh {
 
 	static int N;
 	static char[][] maps;
-	static int[] dx = {-1,1};
-	static int[] dy = {1,1};
-	static class Node implements Comparable<Node>{
-		int y,x,dir,cnt;
+	static int[][][] distance;
 
-		public Node(int y, int x, int dir, int cnt) {
+	static class Mirror implements Comparable<Mirror> {
+		int y, x, dir, cnt;
+
+		public Mirror(int y, int x, int dir, int cnt) {
 			this.y = y;
 			this.x = x;
 			this.dir = dir;
@@ -29,70 +29,104 @@ public class Sh {
 		}
 
 		@Override
-		public int compareTo(Node o) {
+		public int compareTo(Mirror o) {
 			return this.cnt - o.cnt;
 		}
-		
-		
+
+		@Override
+		public String toString() {
+			return "Mirror [y=" + y + ", x=" + x + ", dir=" + dir + ", cnt=" + cnt + "]";
+		}
+
 	}
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		maps = new char[N][N];
-		
+		distance = new int[N][N][4];
 		boolean findStart = false;
-		Node start = null;
-		Node end = null;
-		for(int i=0; i<N; i++) {
+		int[] start = null;
+		int[] end = null;
+		
+		for (int i = 0; i < N; i++) {
 			String str = br.readLine();
-			for(int j=0; j<N; j++) {
+			for (int j = 0; j < N; j++) {
 				maps[i][j] = str.charAt(j);
-				if(!findStart && maps[i][j] == '#')
-					start = new Node(i,j,-1,0);
-				else if(findStart && maps[i][j] == '#')
-					end = new Node(i,j,-1,0);
-			}	
+				if (!findStart && maps[i][j] == '#') {
+					start = new int[] { i, j };
+					findStart = true;
+				} else if (findStart && maps[i][j] == '#')
+					end = new int[] { i, j };
+				for(int k = 0; k<4; k++)
+					distance[i][j][k] = Integer.MAX_VALUE; 
+			}
 		}
+
 		
-		int answer = bfs(start,end);
+		int answer = bfs(start, end);
 		System.out.println(answer);
-		
+
 	}
-	private static int bfs(Node start, Node end) {
-		Queue<Node> pq = new PriorityQueue<>();
-		pq.offer(start);
+
+	private static int bfs(int[] start, int[] end) {
+
+		int[] dx = { 1, -1, 0, 0 };
+		int[] dy = { 0, 0, 1, -1 };
+		Queue<Mirror> pq = new PriorityQueue<>();
+
+		for (int i = 0; i < 4; i++) {
+			pq.offer(new Mirror(start[0], start[1], i, 0));
+			distance[start[0]][start[1]][i] = 0 ;
+		}
+	
 		
-		while(!pq.isEmpty()) {
-			Node cur = pq.poll();
-			int y = cur.y;
-			int x = cur.x;
-			int dir = cur.dir;
-			int cnt = cur.cnt;
-			for(int i=0; i<2; i++) {
-				int ny = y + dy[i];
-				int nx = x + dx[i];
-				
-				if(ny >= N || ny < 0 || nx >= N || nx < 0)
-					continue;
-				if(maps[ny][nx] == '*')
-					continue;
-				
-				if(maps[ny][nx] == '.')
-					pq.offer(new Node(ny,nx,dir,cnt));
-				else if(maps[ny][nx] == '!') {
-					if(dir != i) {
-						pq.offer(new Node(ny,nx,i,cnt+1));
-					}
-					else
-						pq.offer(new Node(ny,nx,i,cnt));
+
+		while (!pq.isEmpty()) {
+			
+			Mirror cur = pq.poll();
+			int cY = cur.y;
+			int cX = cur.x;
+			int cD = cur.dir;
+			int cC = cur.cnt;
+
+			if (cY == end[0] && cX == end[1]) {
+				return cC;
+			}
+
+			int nY = cY + dy[cD];
+			int nX = cX + dx[cD];
+
+			if (nY >= N || nY < 0 || nX >= N || nX < 0)
+				continue;
+			if (maps[nY][nX] == '*')
+				continue;
+			else if (maps[nY][nX] == '.') {
+				if(cC < distance[nY][nX][cD]) {
+					distance[nY][nX][cD] = cC;
+					pq.offer(new Mirror(nY, nX, cD, cC));
 				}
 				
-				if(maps[ny][nx] == '#')
-					return cnt;
 			}
-			
+			else {
+				if(cC < distance[nY][nX][cD]) {
+					distance[nY][nX][cD] = cC;
+					pq.offer(new Mirror(nY, nX, cD, cC));
+					if (cD < 2) {
+						pq.offer(new Mirror(nY, nX, 2, cC + 1));
+						pq.offer(new Mirror(nY, nX, 3, cC + 1));
+					} else {
+						pq.offer(new Mirror(nY, nX, 0, cC + 1));
+						pq.offer(new Mirror(nY, nX, 1, cC + 1));
+					}
+				}
+				
+				
+			}
+
 		}
+
 		return 0;
 	}
 }
